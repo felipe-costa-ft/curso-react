33d45132fe5aac3f0234b830b676b83382939275
felipe-costa-ft <felipecosta@ieee.org>
2021-08-02 11:01:03 -0300

    Capitulo useReducer#1

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -4,7 +4,8 @@ import React from 'react'
 
 import App from './views/App'
 
+
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+        <App />
+    , document.getElementById('root')
 )
